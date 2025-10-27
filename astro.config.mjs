585ead355747f9b7c9b6736d854de045febb2370
lucindasouchet <lucinda.souchet@gmail.com>
2025-10-27 03:32:32 +0100

    uninstalled @astrojs/netlify package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
+// import netlify from '@astrojs/netlify';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: netlify(),
+  // adapter: netlify(),
   vite: {
     plugins: [tailwindcss()]
   },
